8c2c8718e255c1889a662091b32a494ad0ac1966
Meredith Jason <56037106+nhatanhnguyenbui@users.noreply.github.com>
2020-12-18 00:17:04 +0700

    Practical work 4

--- USTHWeather2/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather2/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -46,6 +46,19 @@ public class ForecastFragment extends Fragment {
         //ll.setOrientation(LinearLayout.VERTICAL);
         View view = new View(getContext());
         view.setBackgroundColor(Color.parseColor("#3b9ae8"));
+
+TextView textView = new TextView(getContext());
+        textView.setText("Thursday");
+
+        ImageView imageView = new ImageView(getContext());
+        imageView.setImageResource(R.drawable.cloud);
+
+        LinearLayout layout = new LinearLayout(getContext());
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.addView(textView);
+        layout.addView(imageView);
+        return inflater.inflate(R.layout.fragment_forecast, container, false);
+
         return view;
     }
 }
